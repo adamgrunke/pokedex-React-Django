e83178abf3c24332afef0a8274eb68d7df3237e3
onoroff <adam.grunke@gmail.com>
2019-08-10 18:59:48 -0700

    Added notes. POST to favorites in process. 400 Bad Request error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,10 @@ function App() {
   const [pokemonName, setPokemonName] = useState(["bulbasaur"])
   const [pokemon, setPokemon] = useState({})
   const [pokemonFavorites, setPokemonFavorites] = useState({})
+  const [addFavPokemon, setAddFavPokemon] = useState();
 
+
+  // GET GET GET - FROM API - Calls for the amount of Pokemon specified from the api.
   useEffect ( () => {
     console.log("running the axios call for 151 pokemon useEffect")
     axios.get('https://pokeapi.co/api/v2/pokemon/').then( (response) => {
@@ -19,14 +22,16 @@ function App() {
     })
   }, [])
 
+  // GET GET GET - FROM DB - This calls for the pokemon that have been stored in the database as favorites.
   useEffect ( () => {
     console.log("running the axios call for favorite pokemon useEffect")
     axios.get('/pokemon/').then( (response) => {
       setPokemonFavorites(response.data);
     
     })
-  },[pokemonFavorites.length])
+  },[pokemonFavorites.length]) // how will it know that the length of the array has changed without calling it??? 
 
+  // GET GET GET - DETAILS FROM API - This request details for a specific pokemon from the api.
   useEffect ( () => {
     console.log("running the axios call for pokemon DETAILS useEffect")
     axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then( (response) => {
@@ -35,12 +40,19 @@ function App() {
     })
   }, [pokemonName])
 
+  // POST POST POST - ADD Favorite to DB - THis useEffect is intended to add a selected pokemon to the favorites list. 
+  useEffect ( () => {
+    console.log("running the axios call to POST a pokemon to fav db useEffect")
+    axios.post('/pokemon/').then( (response) => {
+      console.log("POSTED??")
+    })
+  },[addFavPokemon])
 
 
 
   return (
     <div className="App">
-      <PokemonDetails pokemonName={pokemonName} pokemon={pokemon} />
+      <PokemonDetails pokemonName={pokemonName} pokemon={pokemon} handleAddFav={setAddFavPokemon} />
       <hr />
       <PokemonFavorites pokemonFavorites={pokemonFavorites} />
       <hr />
--- client/src/components/PokemonDetails.jsx
+++ client/src/components/PokemonDetails.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function PokemonDetails({pokemonName, pokemon}) {
+function PokemonDetails({pokemonName, pokemon, handleAddFav}) {
     let content;
     // console.log("from Details", pokemonName)
     // console.log(pokemon)
@@ -11,10 +11,12 @@ function PokemonDetails({pokemonName, pokemon}) {
             <h1>Deh-tAY-ls</h1>
             <h3>{pokemonName}</h3>
             <p>Height: {pokemon.height}</p>
-            <button>Add to favorites!!!</button>
+            <button onClick={ () => handleAddFav(pokemonName)}>Add to favorites!!!</button>
         </>
     )
 
+    // when this button is selected, the add favorite state is changed. This change will trigger
+    // the POST to the db with the NAME of the pokemon that is saved 
 
 
     return(
